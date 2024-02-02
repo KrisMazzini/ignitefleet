e71f18226793ae2ef41d473dee22a3cf395d23d8
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2024-02-02 18:25:25 -0300

    feat(fonts): config app fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,29 @@
+/* eslint-disable camelcase */
+import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
 import theme from './src/theme'
+
 import { SignIn } from './src/screens/SignIn'
+import { Loading } from './src/components/Loading'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <SignIn />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
     </ThemeProvider>
   )
 }
